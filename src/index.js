760ebb52f22380fc5d95caf02068febcbb30c9f2
Markus Andersson <make_99@hotmail.com>
2018-07-16 19:03:55 +0200

    minor tweeks still not working

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
-import HomePage from "./components/pages/HomePage";
-import LoginPage from "./components/pages/LoginPage";
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
