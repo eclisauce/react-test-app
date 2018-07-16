6bb3adf12cede615a97a50587afb2ac5bd6d0c1f
Markus Andersson <make_99@hotmail.com>
2018-07-16 18:56:45 +0200

    Started with from and added code for it
    Some problem and it's not rendering as it should

---
+++ src/components/forms/LoginForm.js
@@ -0,0 +1,53 @@
+import React from "react";
+import {Form, Button} from "semantic-ui-react";
+
+class LoginForm extends React.Component {
+  state = {
+    data: {
+      email: "",
+      password: ""
+    },
+    loading: false,
+    errors: {}
+  };
+
+onChange = e =>
+this.setState({
+  data: {...this.state.data, [e.target.name]: e.target.value}
+});
+
+  render(){
+
+    const {data} = this.state;
+
+    return (
+      <Form>
+      <Form.field>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="example@example.com"
+          value={data.email}
+          onChange={this.onChange}
+        />
+      </Form.field>
+      <Form.field>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Make it secure"
+          value={data.password}
+          onChange={this.onChange}
+        />
+      </Form.field>
+        <Button primary>Login</Button>
+      </Form>
+    );
+  }
+}
+
+export default LoginForm;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
