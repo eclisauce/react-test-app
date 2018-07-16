908f9edc8a9ffa78a90f8b31658e0f43f2e4ae00
Markus Andersson <make_99@hotmail.com>
2018-07-16 23:59:22 +0200

    Login form with client response working

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,5 +1,8 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import {Form, Button} from "semantic-ui-react";
+import Validator from "validator";
+import InlineError from '../messages/InlineError';
 
 class LoginForm extends React.Component {
   state = {
@@ -16,24 +19,40 @@ this.setState({
   data: {...this.state.data, [e.target.name]: e.target.value}
 });
 
+onSubmit = () =>{
+  const errors = this.validate(this.state.data);
+  this.setState({errors});
+  if (Object.keys(errors).length === 0){
+    this.props.submit(this.state.data);
+  }
+}
+
+validate = (data) => {
+  const errors = {};
+  if (!Validator.isEmail(data.email)) errors.email = "Invalid email"
+  if (!data.password) errors.password = "Can't be blank";
+  return errors;
+}
+
   render(){
 
-    const {data} = this.state;
+    const {data, errors} = this.state;
 
     return (
-      <Form>
-      <Form.field>
+      <Form onSubmit={this.onSubmit}>
+      <Form.Field error={!!errors.email}>
         <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="example@example.com"
-          value={data.email}
-          onChange={this.onChange}
-        />
-      </Form.field>
-      <Form.field>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="example@example.com"
+            value={data.email}
+           onChange={this.onChange}
+         />
+       {errors.email && <InlineError text={errors.email} />}
+     </Form.Field>
+      <Form.Field error={!!errors.password}>
         <label htmlFor="password">Password</label>
         <input
           type="password"
@@ -43,11 +62,16 @@ this.setState({
           value={data.password}
           onChange={this.onChange}
         />
-      </Form.field>
+      {errors.password && <InlineError text={errors.password} />}
+    </Form.Field>
         <Button primary>Login</Button>
       </Form>
     );
   }
 }
+
+LoginForm.proptypes = {
+  submit: PropTypes.func.isRequired
+};
 
 export default LoginForm;
